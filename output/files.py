9c5a52713171aac2673d5aea0e86f108957cb1a1
Benjamin Moody <benjaminmoody@gmail.com>
2018-01-02 20:27:07 -0500

    Add ArchiveBinaryFile class.
    This class offers random, and hopefully efficient, write access to a
    binary file.  Intended for writing signal files.  Not actually sure
    whether I'm going to use this in the final WaveSampleHandler, but I
    went to the effort of writing it, so there. :)

--- output/files.py
+++ output/files.py
@@ -18,6 +18,7 @@
 
 import os
 import errno
+import mmap
 
 class ArchiveLogFile:
     def __init__(self, filename):
@@ -48,3 +49,66 @@ class ArchiveLogFile:
     def close(self):
         self.flush()
         self.fp.close()
+
+class ArchiveBinaryFile:
+    def __init__(self, filename, window_size = None):
+        # Open the file R/W and create if missing, never truncate
+        self.fd = os.open(filename, os.O_RDWR|os.O_CREAT, 0o666)
+
+        self.current_size = os.lseek(self.fd, 0, os.SEEK_END)
+        self.real_size = self.current_size
+
+        self.window_size = mmap.PAGESIZE * 2
+        if window_size is not None:
+            while self.window_size < window_size:
+                self.window_size *= 2
+
+        self.map_start = self.map_end = 0
+        self.map_buffer = None
+
+    def _map_range(self, start, end):
+        if end < self.map_start or start >= self.map_end:
+            start -= start % mmap.PAGESIZE
+            if end < start + self.window_size:
+                end = start + self.window_size
+            else:
+                end += mmap.PAGESIZE - (end % mmap.PAGESIZE)
+            if end > self.current_size:
+                os.ftruncate(self.fd, end)
+                self.current_size = end
+            self.map_buffer = mmap.mmap(self.fd, end - start, offset = start)
+            self.map_start = start
+            self.map_end = end
+
+    def size(self):
+        return self.real_size
+
+    def truncate(self, size):
+        self.real_size = size
+
+    def write(self, pos, data, mask = None):
+        end = pos + len(data)
+        if end > self.real_size:
+            self.real_size = end
+        self._map_range(pos, end)
+        i = pos - self.map_start
+        if mask is None:
+            self.map_buffer[i : i + len(data)] = data
+        else:
+            for j in range(len(data)):
+                self.map_buffer[i + j] = ((self.map_buffer[i + j] & ~mask[j])
+                                          | (data[j] & mask[j]))
+
+    def flush(self):
+        self.map_start = self.map_end = 0
+        if self.map_buffer is not None:
+            self.map_buffer.close()
+            self.map_buffer = None
+        if self.real_size != self.current_size:
+            os.ftruncate(self.fd, self.real_size)
+            self.current_size = self.real_size
+        os.fdatasync(self.fd)
+
+    def close(self):
+        self.flush()
+        os.close(self.fd)
