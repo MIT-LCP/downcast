98fcd5af595efbfe7480c2c445d8a08a60377ecc
Benjamin Moody <benjaminmoody@gmail.com>
2018-06-05 15:28:43 -0400

    WorkerProcess: new class.
    This will be used in order to finalize records in the background.

---
+++ downcast/output/process.py
@@ -0,0 +1,54 @@
+#
+# downcast - tools for unpacking patient data from DWC
+#
+# Copyright (c) 2018 Laboratory for Computational Physiology
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+import cProfile
+from multiprocessing import Process
+
+class WorkerProcess(Process):
+    def __init__(self, name = None, keep_files = None, **kwargs):
+        Process.__init__(self, name = name, **kwargs)
+        if keep_files is None:
+            keep_files = [sys.stdin, sys.stdout, sys.stderr]
+        self.keep_fds = {x.fileno() for x in keep_files}
+
+    def run(self):
+        # Close all files except those listed in keep_files
+        fds = []
+        for name in os.listdir('/dev/fd'):
+            try:
+                fds.append(int(name))
+            except ValueError:
+                pass
+
+        for fd in fds:
+            if fd not in self.keep_fds:
+                try:
+                    os.close(fd)
+                except OSError:
+                    pass
+
+        # Invoke the target function, with profiling if enabled
+        pf = os.environ.get('DOWNCAST_PROFILE_OUT', None)
+        name = self.name
+        if pf is not None and name is not None:
+            pf = '%s.%s' % (pf, name)
+            cProfile.runctx('Process.run(self)', globals(), locals(), pf)
+        else:
+            Process.run(self)
