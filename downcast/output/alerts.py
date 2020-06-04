ff194270024a431142957f6f7660004857032b67
Benjamin Moody <benjaminmoody@gmail.com>
2020-06-04 16:13:10 -0400

    AlertFinalizer: write alerts to an annotation file.
    Alarm annotations include the onset, announcement, and end of the
    alarm.  An additional annotation is added if the message or severity
    of an alarm changes, or the alarm becomes silenced or un-silenced.
    
    The onset time, if known, should be close to the exact time of the
    triggering event.  How precise the other timestamps are is unclear.
    
    Since there may be multiple alarms active at a time, a unique integer
    ID is used to identify each alarm and included (enclosed in braces) in
    the annotation aux string.

--- downcast/output/alerts.py
+++ downcast/output/alerts.py
@@ -17,10 +17,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from datetime import datetime, timezone
+import os
+import re
 
 from ..messages import AlertMessage
 from ..timestamp import (T, delta_ms)
 from ..util import string_to_ascii
+from .wfdb import (Annotator, AnnotationType)
 
 _sane_time = T('1970-01-01 00:00:00.000 +00:00')
 
@@ -80,25 +83,133 @@ class AlertFinalizer:
         self.record = record
         self.log = record.open_log_reader('_phi_alerts', allow_missing = True)
 
-        # Scan the alerts log file, and add timestamps to the time map
+        self.alert_onset = {}
+        self.alert_announce = {}
+        self.alert_end = {}
+
+        # Scan the alerts log file, add timestamps to the time map,
+        # and record onset/announce/end time for each alert ID.
         for (sn, ts, line) in self.log.unsorted_items():
             ts = datetime.strptime(str(ts), '%Y%m%d%H%M%S%f')
             ts = ts.replace(tzinfo = timezone.utc)
             record.time_map.add_time(ts)
 
+            (alert_id, event, severity, state, label) = _parse_info(line)
+            if event == b'!':
+                if ts < self.alert_onset.setdefault(alert_id, ts):
+                    self.alert_onset[alert_id] = ts
+            elif event == b'+':
+                if ts < self.alert_announce.setdefault(alert_id, ts):
+                    self.alert_announce[alert_id] = ts
+            elif event == b'-':
+                if ts > self.alert_end.setdefault(alert_id, ts):
+                    self.alert_end[alert_id] = ts
+
     def finalize_record(self):
         sn0 = self.record.seqnum0()
+        if sn0 is None:
+            # if we don't have a seqnum0 then time is meaningless
+            return
 
-        if not self.log.missing():
-            af = self.record.open_log_file('alerts')
+        alert_first = {}
+        alert_last = {}
+        alert_num = {}
+
+        annfname = os.path.join(self.record.path, 'waves.alarm')
+        with Annotator(annfname, afreq = 1000) as anns:
+            # Reread the alerts log file in order.  Assign an integer
+            # ID to each alert in order of appearance, and record the
+            # severity, state (silenced or not) and label.  If the
+            # severity/state/label changes between the announce time
+            # and end time, then add an annotation each time it
+            # changes.  The earliest severity/state/label will be
+            # applied to the announce and onset annotations, and the
+            # latest severity/state/label will be applied to the end
+            # annotation.
             for (sn, ts, line) in self.log.sorted_items():
                 if b'\030' in line:
                     continue
                 ts = datetime.strptime(str(ts), '%Y%m%d%H%M%S%f')
                 ts = ts.replace(tzinfo = timezone.utc)
                 sn = self.record.time_map.get_seqnum(ts) or sn
-                if sn0 is None:
-                    sn0 = sn
-                af.fp.write(('%s\t' % (sn - sn0)).encode()) # XXX
-                af.fp.write(line.strip())                   # XXX
-                af.fp.write(b'\n')                          # XXX
+                t = sn - sn0
+
+                (alert_id, event, severity, state, label) = _parse_info(line)
+                if alert_id and label:
+                    num = alert_num.setdefault(alert_id, len(alert_num) + 1)
+                    oldstate = alert_last.get(alert_id, None)
+                    newstate = (severity, state, label)
+                    alert_first.setdefault(alert_id, newstate)
+                    alert_last[alert_id] = newstate
+                    announce = self.alert_announce.get(alert_id)
+                    end = self.alert_end.get(alert_id)
+                    if (oldstate and oldstate != newstate
+                          and (not announce or ts > announce)
+                          and (not end or ts < end)):
+                        _put_annot(anns, t, num, b';', severity, state, label)
+
+            for (alert_id, ts) in self.alert_onset.items():
+                num = alert_num.get(alert_id)
+                sn = self.record.time_map.get_seqnum(ts)
+                if num is None or sn is None:
+                    continue
+                t = sn - sn0
+                (severity, state, label) = alert_first[alert_id]
+                _put_annot(anns, t, num, b'+', severity, state, label)
+
+            for (alert_id, ts) in self.alert_announce.items():
+                num = alert_num.get(alert_id)
+                sn = self.record.time_map.get_seqnum(ts)
+                if num is None or sn is None:
+                    continue
+                t = sn - sn0
+                (severity, state, label) = alert_first[alert_id]
+                _put_annot(anns, t, num, b'<', severity, state, label)
+
+            for (alert_id, ts) in self.alert_end.items():
+                num = alert_num.get(alert_id)
+                sn = self.record.time_map.get_seqnum(ts)
+                if num is None or sn is None:
+                    continue
+                t = sn - sn0
+                (severity, state, label) = alert_last[alert_id]
+                _put_annot(anns, t, num, b'>', severity, state, label)
+
+_info_pattern = re.compile(rb'\(([\w-]+)\)(?:([-+!])|(\d+)([=~])(.*))')
+
+def _parse_info(line):
+    m = _info_pattern.fullmatch(line.rstrip(b'\n'))
+    if m:
+        return m.groups()
+    else:
+        return (None, None, None, None, None)
+
+def _put_annot(anns, time, alert_num, event_code, severity, state, label):
+    severity = int(severity)
+    if severity == 0:           # RED
+        subtyp = 3
+    elif severity == 1:         # YELLOW
+        subtyp = 2
+    elif severity == 2:         # SHORT YELLOW
+        subtyp = 1
+    else:
+        subtyp = 0
+
+    if event_code == b'+':      # onset
+        subtyp += 90
+    elif event_code == b'<':    # announce
+        subtyp += 80
+    elif event_code == b'>':    # end
+        subtyp += 60
+    else:
+        subtyp += 70
+
+    aux = event_code + b'{' + str(alert_num).encode() + b'}'
+    if state == b'~':           # silenced
+        aux += b'~'
+    else:
+        aux += b' '
+    aux += label
+
+    anns.put(time = time, anntyp = AnnotationType.NOTE,
+             subtyp = subtyp, chan = 255, aux = aux)
