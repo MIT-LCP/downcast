1fe7a9724166d44af98e4c8a558e8a9103772e6d
Benjamin Moody <benjaminmoody@gmail.com>
2021-11-22 09:47:50 -0500

    For aperiodics, use sequence number as observation time.
    For "periodic" numerics, assume that the timestamp in the
    NumericValueMessage provides a precise observation time (once it is
    translated via the time map.)  Note that btbHR is considered
    "periodic".
    
    For "aperiodic" numerics, ignore the timestamp and just use the
    sequence number as a coarse observation time.
    
    This is possibly still wrong.

--- downcast/output/numerics.py
+++ downcast/output/numerics.py
@@ -17,6 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from datetime import datetime, timezone
+import heapq
 
 from ..messages import NumericValueMessage
 from ..util import string_to_ascii
@@ -24,7 +25,8 @@ from ..util import string_to_ascii
 class NumericValueHandler:
     def __init__(self, archive):
         self.archive = archive
-        self.last_event = {}
+        self.last_periodic = {}
+        self.last_aperiodic = {}
 
     def send_message(self, chn, msg, source, ttl):
         if not isinstance(msg, NumericValueMessage):
@@ -51,28 +53,50 @@ class NumericValueHandler:
             source.ack_message(chn, msg, self)
             return
 
-        # Open or create a log file
-        logfile = record.open_log_file('_phi_numerics')
+        if attr.is_aperiodic:
+            # Open or create a log file
+            logfile = record.open_log_file('_phi_aperiodics')
 
-        # Write the sequence number and timestamp to the log file
-        # (if they don't differ from the previous event)
-        sn = msg.sequence_number
-        ts = msg.timestamp
-        (old_sn, old_ts) = self.last_event.get(record, (None, None))
-        if sn != old_sn:
-            logfile.append('S%s' % sn)
-        if ts != old_ts:
-            logfile.append(ts.strftime_utc('%Y%m%d%H%M%S%f'))
-        self.last_event[record] = (sn, ts)
+            # Write the sequence number to the log file
+            # (if it doesn't differ from the previous event)
+            sn = msg.sequence_number
+            old_sn = self.last_aperiodic.get(record, None)
+            if sn != old_sn:
+                logfile.append('S%s' % sn)
+            self.last_aperiodic[record] = sn
 
-        # Write the value to the log file
-        lbl = string_to_ascii(attr.sub_label)
-        ulbl = string_to_ascii(attr.unit_label)
-        val = msg.value
-        if val is None:
-            val = ''
-        logfile.append('%s\t%s\t%s' % (lbl, val, ulbl))
-        source.ack_message(chn, msg, self)
+            # Write the value to the log file
+            lbl = string_to_ascii(attr.sub_label)
+            ulbl = string_to_ascii(attr.unit_label)
+            val = msg.value
+            if val is None:
+                val = ''
+            logfile.append('%s\t%s\t%s' % (lbl, val, ulbl))
+            source.ack_message(chn, msg, self)
+
+        else:
+            # Open or create a log file
+            logfile = record.open_log_file('_phi_numerics')
+
+            # Write the sequence number and timestamp to the log file
+            # (if they don't differ from the previous event)
+            sn = msg.sequence_number
+            ts = msg.timestamp
+            (old_sn, old_ts) = self.last_periodic.get(record, (None, None))
+            if sn != old_sn:
+                logfile.append('S%s' % sn)
+            if ts != old_ts:
+                logfile.append(ts.strftime_utc('%Y%m%d%H%M%S%f'))
+            self.last_periodic[record] = (sn, ts)
+
+            # Write the value to the log file
+            lbl = string_to_ascii(attr.sub_label)
+            ulbl = string_to_ascii(attr.unit_label)
+            val = msg.value
+            if val is None:
+                val = ''
+            logfile.append('%s\t%s\t%s' % (lbl, val, ulbl))
+            source.ack_message(chn, msg, self)
 
     def flush(self):
         self.archive.flush()
@@ -80,16 +104,26 @@ class NumericValueHandler:
 class NumericValueFinalizer:
     def __init__(self, record):
         self.record = record
-        self.log = record.open_log_reader('_phi_numerics',
-                                          allow_missing = True)
 
-        # Scan the numerics log file; make a list of all non-null
+        # Scan the log files; make a list of all non-null
         # numerics, and add timestamps to the time map
         self.all_numerics = set()
-        for (sn, ts, line) in self.log.unsorted_items():
+
+        self.periodic_log = record.open_log_reader('_phi_numerics',
+                                                   allow_missing = True)
+        for (sn, ts, line) in self.periodic_log.unsorted_items():
             ts = datetime.strptime(str(ts), '%Y%m%d%H%M%S%f')
             ts = ts.replace(tzinfo = timezone.utc)
             record.time_map.add_time(ts)
+            if b'\030' not in line:
+                parts = line.rstrip(b'\n').split(b'\t')
+                # ignore nulls
+                if len(parts) >= 3 and parts[1]:
+                    self.all_numerics.add((parts[0], parts[2]))
+
+        self.aperiodic_log = record.open_log_reader('_phi_aperiodics',
+                                                    allow_missing = True)
+        for (sn, _, line) in self.aperiodic_log.unsorted_items():
             if b'\030' not in line:
                 parts = line.rstrip(b'\n').split(b'\t')
                 # ignore nulls
@@ -111,7 +145,9 @@ class NumericValueFinalizer:
             cur_ts = None
             cur_sn = None
             cur_time = None
-            for (sn, ts, line) in self.log.sorted_items():
+            for (sn, ts, line) in heapq.merge(
+                    self.periodic_log.sorted_items(),
+                    self.aperiodic_log.sorted_items()):
                 if b'\030' in line:
                     continue
                 parts = line.rstrip(b'\n').split(b'\t')
@@ -122,16 +158,27 @@ class NumericValueFinalizer:
 
                 # determine new time value
                 if ts == cur_ts and sn == cur_sn:
-                    time = cur_values[0]
+                    time = cur_time
                 else:
-                    ts = datetime.strptime(str(ts), '%Y%m%d%H%M%S%f')
-                    ts = ts.replace(tzinfo = timezone.utc)
-                    sn = self.record.time_map.get_seqnum(ts, sn + 5120) or sn
+                    if ts == 0:
+                        # for aperiodics (such as NBP), use sequence number as
+                        # observation time
+                        obs_sn = sn
+                    else:
+                        # for periodics, translate timestamp to
+                        # sequence number and use that as observation
+                        # time
+                        ts = datetime.strptime(str(ts), '%Y%m%d%H%M%S%f')
+                        ts = ts.replace(tzinfo = timezone.utc)
+                        obs_sn = self.record.time_map.get_seqnum(ts, sn + 5120)
+                        if obs_sn is None:
+                            obs_sn = sn
+
                     if sn0 is None:
-                        sn0 = sn
+                        sn0 = obs_sn
                     # Time measured in counter ticks, ick.
                     # Better would probably be to use (real) seconds
-                    time = str(sn - sn0).encode()
+                    time = str(obs_sn - sn0).encode()
                     cur_ts = ts
                     cur_sn = sn
                     cur_time = time
